5bc7bb30130176eb5c6a87c41df264a35b3fdc3b
mdmunna84880 <mdmunna19434@gmail.com>
2026-02-16 07:57:10 +0530

    Remove some secret from the code and shifted to env.

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzTL6s2n7YAmH-5fJbuuGpITsRE084Zk8",
-  authDomain: "medibook-4171a.firebaseapp.com",
-  projectId: "medibook-4171a",
-  storageBucket: "medibook-4171a.firebasestorage.app",
-  messagingSenderId: "651124927009",
-  appId: "1:651124927009:web:cd0377a68da0760d1e9160",
-  measurementId: "G-ZRNG95ZF9Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
